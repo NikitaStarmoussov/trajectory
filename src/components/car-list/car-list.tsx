223e3caef98a666fac50e08432a9f3995daf467f
NikitaStarmoussov <nikta.starmousov@gmail.com>
2024-07-08 18:04:44 +0300

    feat: add basic response

--- src/components/car-list/car-list.tsx
+++ src/components/car-list/car-list.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Car } from "../../types/car";
 import { CarItem } from "../car-item/car-item";
 import { Filters } from "../filters/filters";
@@ -19,40 +19,73 @@ export const CarContext = createContext<CarContextProps>({
 });
 
 export const CarList: React.FC = () => {
-  const initialCars: Car[] = [
-    {
-      id: 1,
-      name: "Toyota",
-      model: "Corolla",
-      year: 2021,
-      color: "Black",
-      price: 25000,
-      latitude: 55.753215,
-      longitude: 37.620393,
-    },
-    {
-      id: 2,
-      name: "Honda",
-      model: "Civic",
-      year: 2020,
-      color: "White",
-      price: 22000,
-      latitude: 0,
-      longitude: 0,
-    },
-    {
-      id: 3,
-      name: "Ford",
-      model: "Mustang",
-      year: 2019,
-      color: "Red",
-      price: 35000,
-      latitude: 0,
-      longitude: 0,
-    },
-  ];
+  const [cars, setCars] = useState<Car[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  // const initialCars: Car[] = [
+  //   {
+  //     id: 1,
+  //     name: "Toyota",
+  //     model: "Corolla",
+  //     year: 2021,
+  //     color: "Black",
+  //     price: 25000,
+  //     latitude: 55.753215,
+  //     longitude: 37.620393,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Honda",
+  //     model: "Civic",
+  //     year: 2020,
+  //     color: "White",
+  //     price: 22000,
+  //     latitude: 0,
+  //     longitude: 0,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Ford",
+  //     model: "Mustang",
+  //     year: 2019,
+  //     color: "Red",
+  //     price: 35000,
+  //     latitude: 0,
+  //     longitude: 0,
+  //   },
+  // ];
 
-  const [cars, setCars] = useState(initialCars);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        // const response = await fetch('/api/cars');
+        const response = await fetch('https://test.tspb.su/test-task/vehicles');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setCars(data);
+        setError(null);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+
 
   const handleDeleteCar = (id: number) => {
     setCars(cars.filter((car) => car.id !== id));
